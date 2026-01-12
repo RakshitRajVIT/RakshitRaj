c33976e832b120051291d97a266729874b7e7d6c
RakshitRajVIT <rakshitraj2323@gmail.com>
2026-01-12 11:05:16 +0530

    done

---
+++ .vite/deps/maath.js
@@ -0,0 +1,26 @@
+import {
+  buffer,
+  easing,
+  geometry,
+  index,
+  matrix,
+  misc,
+  three,
+  triangle,
+  vector2,
+  vector3
+} from "./chunk-2M4ZTJBN.js";
+import "./chunk-DXMV2PVU.js";
+import "./chunk-G3PMV62Z.js";
+export {
+  buffer,
+  easing,
+  geometry,
+  matrix,
+  misc,
+  index as random,
+  three,
+  triangle,
+  vector2,
+  vector3
+};
